4318046f7cee396d3c82bb065a390e7d41bbdb99
badFarmer <guy@strategicmfg.com>
2019-01-06 21:05:05 -0800

    Initial Commit

---
+++ program.py
@@ -0,0 +1,17 @@
+def main():
+    pass
+
+
+
+
+
+def print_header():
+    print('------------------------')
+    print('------WIZARD BASH-------')
+    print('------------------------')
+
+
+if __name__ == '__main__':
+    main()
+
+
