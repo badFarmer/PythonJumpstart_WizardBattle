02b3a203cf787644baab68b1fb7afafc96a7c037
badFarmer <guy@strategicmfg.com>
2019-01-06 21:10:01 -0800

    Incremental Commit

--- program.py
+++ program.py
@@ -1,14 +1,27 @@
 def main():
-    pass
-
-
-
+    print_header()
 
 
 def print_header():
     print('------------------------')
     print('------WIZARD BASH-------')
     print('------------------------')
+
+
+def game_loop():
+    while True:
+
+        cmd = input('Do you attack, runaway, or look around?')
+        if cmd == 'a':
+            print('attack')
+        elif cmd == 'r':
+            print('runaway')
+        elif cmd == 'l':
+            print('look around.')
+        else:
+            print('OK, exiting game... bye!')
+            break
+
 
 
 if __name__ == '__main__':
